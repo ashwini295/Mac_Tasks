f6a3fe4dcf78528f634ef17596b5347ebc16d062
Ashwini <ashwini.h209@gmail.com>
2024-10-14 07:00:08 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/Octex_11102024_Operatores/Lab051.java
@@ -0,0 +1,12 @@
+package Octex_11102024_Operatores;
+
+public class Lab051 {
+
+    public static void main(String[] args) {
+        char a1='A';
+        char b1='B';
+        System.out.println(a1+b1);
+
+        //concatination happens with char then Ascii int value should be taken so o/p will be 131
+    }
+}
